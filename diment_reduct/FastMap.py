c2ed92b483f1b2247719d645980973b20bf1d36e
Jingfeng <renjff@gmail.com>
2017-10-11 14:23:56 -0700

    fastmap with output shown in plot

--- diment_reduct/FastMap.py
+++ diment_reduct/FastMap.py
@@ -8,17 +8,18 @@ raw_points = readfile.read_points('fastmap-data.txt')
 
 def fast_map(point_list):
     # step1: identify the furthest pair
-    fur_dist = 0
+    fur_dist = -1
     for each_pair in point_list:
         if each_pair[2] > fur_dist:
             fur_pair = each_pair
             fur_dist = each_pair[2]
         elif each_pair[2] == fur_dist:
-            if (each_pair[0] + each_pair[1]) < (fur_pair[0] + fur_pair[0]):
+            if (each_pair[0] + each_pair[1]) < (fur_pair[0] + fur_pair[1]):
                 fur_pair = each_pair
     #print(fur_pair, fur_dist)
 
     # step2:
+    cord_set = {}
     a = min(fur_pair[0],fur_pair[1])
     b = max(fur_pair[0],fur_pair[1])
     d_ab = fur_dist
@@ -27,22 +28,30 @@ def fast_map(point_list):
         i = each_pair[0]
         j = each_pair[1]
         d_ij = each_pair[2]
-        xi = cal_cord(i, a, b, d_ab)
-        xj = cal_cord(j, a, b, d_ab)
+
+        if i in cord_set:
+            xi = cord_set[i]
+        else:
+            xi = cal_cord(i, a, b, d_ab,point_list)
+            cord_set.update({i: xi})
+
+        if j in cord_set:
+            xj = cord_set[j]
+        else:
+            xj = cal_cord(j, a, b, d_ab,point_list)
+            cord_set.update({j: xj})
+
         d_ij_new = np.sqrt(d_ij ** 2 - (xi - xj) ** 2)
         new_pointlist.append([i, j, d_ij_new])
-    #print(new_pointlist)
-    #fast_map(new_pointlist)
-    plot_words(new_pointlist, word_list)
+    return cord_set,new_pointlist
 
-
-def cal_cord(i, a, b, d_ab):
+def cal_cord(i, a, b, d_ab,point_list):
     if i == a:
         first_cord = 0
     elif i == b:
         first_cord = d_ab
     else:
-        for each_item in raw_points:
+        for each_item in point_list:
             if each_item[0] == i and each_item[1] == a:
                 d_ai = each_item[2]
             if each_item[0] == a and each_item[1] == i:
@@ -52,15 +61,26 @@ def cal_cord(i, a, b, d_ab):
             if each_item[0] == b and each_item[1] == i:
                 d_bi = each_item[2]
         first_cord = (d_ai ** 2 + d_ab ** 2 - d_bi ** 2) / (2 * d_ab)
-        print(first_cord)
+        #print(first_cord)
     return first_cord
 
-def plot_words(plot_points, plot_text):
+def plot_words(cord_1,cord_2, plot_text):
     plt.figure
+    plt.scatter(cord_1,cord_2)
+    for i in range(len(cord_1)):
+        plt.annotate(plot_text[i], xy=(cord_1[i], cord_2[i]))
+    #plt.text(cord_1,cord_2,plot_text)
+    plt.show()
+    plt.close()
 
 def main():
     point_list = raw_points[:]
-    fast_map(point_list)
+    cord_1,next_points=fast_map(point_list)
+    cord_2=fast_map(next_points)[0]
+    print(cord_1)
+    print(cord_2)
+    plot_words(list(cord_1.values()),list(cord_2.values()),word_list)
+    return cord_1,cord_2
 
 
 main()
--- diment_reduct/pca.py
+++ diment_reduct/pca.py
@@ -1,7 +1,7 @@
 import numpy as np
 import readfile
 
-def cal_pca(n=3,k=2):
+def cal_pca(k,n=3):
     point_list = readfile.read_points('pca-data.txt')
     point_mat = np.mat(point_list)
     #step 1
@@ -12,15 +12,16 @@ def cal_pca(n=3,k=2):
     e_value,e_vector = np.linalg.eig(sigma)
     #step 3 ??
     u_nn = e_vector[:]
-    u_kn = u_nn[:k]
-    print(u_nn)
+    u_kn = u_nn.T[:k].T
+    print(u_kn)
     #step 4
     z_list = []
-    for i in range(len(point_list)):
-        xi = point_list[i]
-        xi_mat = np.mat(xi).T
-        zi = u_kn * xi_mat
-        z_list.append(zi.T.tolist())
+    # for i in range(len(point_list)):
+    #     xi = point_list[i]
+    #     xi_mat = np.mat(xi).T
+    #     zi = u_kn * xi_mat
+    #     z_list.append(zi.T.tolist())
     #print(z_list)
+    return u_kn
 
-cal_pca()
+cal_pca(2)
